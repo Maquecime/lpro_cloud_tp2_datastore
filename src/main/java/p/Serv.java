f1912058c9114a27f30e948baea5e1ef3e8558c1
Maquecime <mdatin.contact@gmail.com>
2020-02-04 11:51:01 +0100

    fix: serv

--- src/main/java/p/Serv.java
+++ src/main/java/p/Serv.java
@@ -8,8 +8,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreException;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.Key;
@@ -22,27 +24,25 @@ import com.google.cloud.datastore.StructuredQuery.OrderBy;
 @Path("/employes")
 public class Serv {
 		
-	Datastore datastore;
-	public Serv() {
-		 Datastore datastore = DatastoreOptions.newBuilder().setProjectId("inf63app10").build().getService();
-
-	} 
-	 
+ 	
 	 @POST
 	 @Consumes("application/json")
 	 public void addEmployee(Employees e) {
 		 String kind = "Employees";
-		 String name = "TestDatastore";
-		 Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(name);
-		 
-		 Entity employee = Entity.newBuilder(taskKey).set("id", taskKey).build();
+		 String id = e.getId();
+		 String name = e.getName();
+		 Datastore datastore = DatastoreOptions.newBuilder().setProjectId("inf63app10").build().getService();
+		 Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(id);
+		 Entity employee = Entity.newBuilder(taskKey).set("name", name).build();
 		 datastore.put(employee);
 	 }
 	 
 	 @GET
 	 @Produces("text/html")
 	 public String getEmployees() {
-		 Query<Entity> query = Query.newEntityQueryBuilder().setKind("Employees").setOrderBy(OrderBy.asc("name")).build();
+		 try {
+		 Datastore datastore = DatastoreOptions.newBuilder().setProjectId("inf63app10").build().getService();
+		 Query<Entity> query = Query.newEntityQueryBuilder().setKind("Employees").build();
 		 Iterator<Entity> it = datastore.run(query);
 		 String ret = "<html><head><meta charset='utf-8'/></head><body><ul>";
 		 while(it.hasNext()) {
@@ -51,6 +51,8 @@ public class Serv {
 		 }
 		 ret+="</ul></body></html>";
 		 return ret;
-	 }
-	
+		 }
+		 catch (DatastoreException e) {System.out.println(e.getReason());
+		 throw new WebApplicationException(503);}
+	 }	
  }
